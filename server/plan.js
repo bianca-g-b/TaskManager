c43e16ee9847c6c145842807c9754044b0396c2c
bianca <bianca.g.buha@gmail.com>
2023-06-09 17:17:10 +0100

    initialised project - added backend as server

---
+++ server/plan.js
@@ -0,0 +1,29 @@
+/*
+PLAN
+Make folder server
+Make a file server.js
+cd to server
+npm init -y - to create package.json
+
+INSTALL DEPENDENCIES:
+ - npm install express - to create server
+ - npm install nodemon - to run server
+ - npm install cors - to allow cross origin requests
+ - npm install dotenv - to store private information => add .env file to .gitignore
+ - npm install pg - to connect to postgresql
+
+ - add "type": "module" to package.json
+ - add "dev": "nodemon -r dotenv/config server.js" to package.json (in scripts)
+
+ ADD FILES:
+- add .gitignore file
+- add .env file
+
+ADD FOLDERS TO CREATE A CLEAR STRUCTURE OF THE PROJECT:
+- add folder "routes" - to store all routes
+- add folder "controllers" - to store all controllers
+- add folder "models" - to store all models
+- add folder "config" - to store all config files
+- add folder "db" - to store db file, that will be used to connect to db
+
+*/
